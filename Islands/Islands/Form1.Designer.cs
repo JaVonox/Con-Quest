827ba7d7de5f26c0872fb89df4bc5eae87b646ae
Jamie Gorman <redsss429@Hotmail.co.uk>
2016-11-11 17:49:02 +0000

    Add files via upload

---
+++ Islands/Islands/Form1.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace Islands
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Map = new System.Windows.Forms.DataGridView();
+            this.MinMap = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.Map)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.MinMap)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Map
+            // 
+            this.Map.AllowUserToAddRows = false;
+            this.Map.AllowUserToDeleteRows = false;
+            this.Map.AllowUserToResizeColumns = false;
+            this.Map.AllowUserToResizeRows = false;
+            this.Map.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.Map.CellBorderStyle = System.Windows.Forms.DataGridViewCellBorderStyle.None;
+            this.Map.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.DisableResizing;
+            this.Map.ColumnHeadersVisible = false;
+            this.Map.EditMode = System.Windows.Forms.DataGridViewEditMode.EditProgrammatically;
+            this.Map.GridColor = System.Drawing.Color.White;
+            this.Map.Location = new System.Drawing.Point(1, 0);
+            this.Map.Name = "Map";
+            this.Map.ReadOnly = true;
+            this.Map.RowHeadersVisible = false;
+            this.Map.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.CellSelect;
+            this.Map.ShowEditingIcon = false;
+            this.Map.Size = new System.Drawing.Size(1353, 737);
+            this.Map.TabIndex = 1;
+            // 
+            // MinMap
+            // 
+            this.MinMap.AllowUserToAddRows = false;
+            this.MinMap.AllowUserToDeleteRows = false;
+            this.MinMap.AllowUserToResizeColumns = false;
+            this.MinMap.AllowUserToResizeRows = false;
+            this.MinMap.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.MinMap.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.MinMap.Location = new System.Drawing.Point(856, 447);
+            this.MinMap.Name = "MinMap";
+            this.MinMap.ReadOnly = true;
+            this.MinMap.Size = new System.Drawing.Size(498, 290);
+            this.MinMap.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(1, 0);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Moar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(1, 29);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(75, 20);
+            this.textBox1.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1354, 733);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.MinMap);
+            this.Controls.Add(this.Map);
+            this.Name = "Form1";
+            this.Text = "MAP";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.Map)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.MinMap)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        internal System.Windows.Forms.DataGridView Map;
+        private System.Windows.Forms.DataGridView MinMap;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+
+    }
+}
+
---
+++ Islands/Islands/Form1.cs
@@ -0,0 +1,491 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Islands
+{
+
+
+using Microsoft.VisualBasic;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data;
+using System.Diagnostics;
+
+public partial class Form1 : Form //add partial?
+{
+	//the horizontal position of the map
+	int mapsizehor = 100;
+	//the vertical position of the map
+	int mapsizever = 100;
+    int landtiles = 0;
+    int oceeantiles = 0;
+	string maptype = "4islands";
+
+    //global images
+    Icon Village_Unclaimed = Properties.Resources.Village_Unclaimed;
+	Random randomizar = new Random();
+    DataGridViewImageColumn imageCol0 = new DataGridViewImageColumn();
+    public Form1()
+   { 
+        InitializeComponent();
+
+    }
+
+	private void Form1_Load(object sender, EventArgs e)
+	{
+
+        for (int i = 0; i <= mapsizehor; i++) //bug here
+        {
+            DataGridViewImageColumn imageCol = new DataGridViewImageColumn();
+            Map.Columns.Insert(i, imageCol);
+        }
+            //Map.ColumnCount = mapsizehor;
+		Map.RowCount = mapsizever;
+
+		for (int i = 0; i <= Map.Rows.Count - 1; i++) {
+			DataGridViewRow r = Map.Rows[i];
+			r.Height = 25;
+		}
+
+		for (int i = 0; i <= Map.Columns.Count - 1; i++) {
+			DataGridViewColumn c = Map.Columns[i];
+			c.Width = 25;
+		}
+
+		for (int i = 0; i <= Map.Rows.Count - 1; i++) {
+			for (int ColNo = 0; ColNo <= Map.ColumnCount - 1; ColNo++) {
+
+				if ((!object.ReferenceEquals(Map.Rows[i].Cells[ColNo].Value, DBNull.Value))) {
+					Map.Rows[i].Cells[ColNo].Style.BackColor = Color.Blue;
+				}
+			}
+		}
+        
+        //this will do the map generation
+
+        generatemap(10);
+        generatemap(100);
+        generatemap(1000);
+        generatemap(10000);
+        generatemap(100000);
+        //generatemap(1000000);
+        //generatemap(10000000);
+        layer2();
+        minimap();
+        layer3();
+        
+	}
+
+    public void minimap()
+    {
+        //generate minimap here
+        MinMap.RowCount = Map.RowCount;
+        MinMap.ColumnCount = Map.ColumnCount;
+
+        for (int i = 0; i <= MinMap.Rows.Count - 1; i++) {
+            DataGridViewRow r = MinMap.Rows[i];
+            r.Height = 1;
+        }
+
+        for (int i = 0; i <= MinMap.Columns.Count - 1; i++) {
+            DataGridViewColumn c = MinMap.Columns[i];
+            c.Width = 1;
+        }
+
+        for (int colcount = 1; colcount <= mapsizever; colcount++) {
+            for (int rocount = 1; rocount <= mapsizehor; rocount++) {
+                //bug here
+                MinMap.Rows[rocount - 1].Cells[colcount - 1].Style.BackColor = Map.Rows[rocount - 1].Cells[colcount - 1].Style.BackColor;
+            }
+        }
+
+
+    }
+
+
+    public void generatemap(int msize)
+	{
+		//snake method of generation
+		//Me.Map.CurrentCell = Me.Map(y1, x1)
+
+		int i1x = 0;
+		int i1y = 0;
+		List<int> snkx = new List<int>();
+		List<int> snky = new List<int>();
+		List<int> allx = new List<int>();
+		List<int> ally = new List<int>();
+
+		i1x = randomizar.Next(10, mapsizehor - 9);
+		i1y = randomizar.Next(10, mapsizever - 9);
+
+		snkx.Insert(0, i1x);
+		snky.Insert(0, i1y);
+
+		Map.Rows[snkx[0]].Cells[snky[0]].Style.BackColor = Color.DarkGreen;
+
+		//10 is tiny island
+        //100 is small island
+        //1000 is large island
+        //10000 is continent
+        //100000 is large continent
+        //1000000 is Mega-Continent
+        //10000000 is Planetary Land Bridge
+
+		//results may vary. alot.
+
+        int i = Convert.ToInt32(msize);
+		int rand = 0;
+		dynamic count = 1;
+		int lastrand = 0;
+
+		//     rand 2
+		//rand 1    rand 3
+		//     rand 4
+
+
+		try {
+			while (i > 0) {
+				rand = randomizar.Next(1, 5);
+				if (rand == 1) {
+					if (lastrand == 1) {
+						i = i + 1;
+					} else {
+						snkx.Insert(count, snkx[count - 1] - 1);
+						snky.Insert(count, snky[count - 1]);
+						lastrand = rand;
+						Map.Rows[snkx[count]].Cells[snky[count]].Style.BackColor = Color.Green;
+						count = count + 1;
+					}
+				} else if (rand == 2) {
+					if (lastrand == 2) {
+						i = i + 1;
+					} else {
+						snky.Insert(count, snky[count - 1] + 1);
+						snkx.Insert(count, snkx[count - 1]);
+						lastrand = rand;
+						Map.Rows[snkx[count]].Cells[snky[count]].Style.BackColor = Color.Green;
+						count = count + 1;
+					}
+				} else if (rand == 3) {
+					if (lastrand == 3) {
+						i = i + 1;
+					} else {
+						snkx.Insert(count, snkx[count - 1] + 1);
+						snky.Insert(count, snky[count - 1]);
+						lastrand = rand;
+						Map.Rows[snkx[count]].Cells[snky[count]].Style.BackColor = Color.Green;
+						count = count + 1;
+					}
+				} else if (rand == 4) {
+					if (lastrand == 4) {
+						i = i + 1;
+					} else {
+						snky.Insert(count, snky[count - 1] - 1);
+						snkx.Insert(count, snkx[count - 1]);
+						lastrand = rand;
+						Map.Rows[snkx[count]].Cells[snky[count]].Style.BackColor = Color.Green;
+						count = count + 1;
+					}
+				}
+				i = i - 1;
+			}
+
+		} catch (Exception ex) {
+			goto Exitry1; // TODO: might not be correct. Was : Exit Try
+		}
+
+        Exitry1:
+
+		//add 3/2 blocks in all directions
+
+		int b = snkx.Count() - 1;
+		int lm = 0;
+
+		while (b > 0) {
+			allx.Insert(lm, snkx[b]);
+			ally.Insert(lm, snky[b]);
+			b = b - 1;
+			lm = lm + 1;
+		}
+
+		dynamic m = 0;
+
+
+		try {
+			while (snkx.Count() != m) {
+				allx.Insert(allx.Count, snkx[m] + 1);
+				Map.Rows[allx[allx.Count() - 1]].Cells[ally[ally.Count() - 1]].Style.BackColor = Color.Green;
+				allx.Insert(allx.Count, snkx[m] + 2);
+				Map.Rows[allx[allx.Count() - 1]].Cells[ally[ally.Count() - 1]].Style.BackColor = Color.Green;
+				allx.Insert(allx.Count, snkx[m] + 3);
+				Map.Rows[allx[allx.Count() - 1]].Cells[ally[ally.Count() - 1]].Style.BackColor = Color.Green;
+				allx.Insert(allx.Count, snkx[m] - 1);
+				Map.Rows[allx[allx.Count() - 1]].Cells[ally[ally.Count() - 1]].Style.BackColor = Color.Green;
+				allx.Insert(allx.Count, snkx[m] - 2);
+				Map.Rows[allx[allx.Count() - 1]].Cells[ally[ally.Count() - 1]].Style.BackColor = Color.Green;
+				allx.Insert(allx.Count, snkx[m] - 3);
+				Map.Rows[allx[allx.Count() - 1]].Cells[ally[ally.Count() - 1]].Style.BackColor = Color.Green;
+				ally.Insert(ally.Count, snky[m] + 1);
+				Map.Rows[ally[ally.Count() - 1]].Cells[allx[allx.Count() - 1]].Style.BackColor = Color.Green;
+				ally.Insert(ally.Count, snky[m] + 2);
+				Map.Rows[ally[ally.Count() - 1]].Cells[allx[allx.Count() - 1]].Style.BackColor = Color.Green;
+				ally.Insert(ally.Count, snky[m] + 3);
+				Map.Rows[ally[ally.Count() - 1]].Cells[allx[allx.Count() - 1]].Style.BackColor = Color.Green;
+				ally.Insert(ally.Count, snky[m] - 1);
+				Map.Rows[ally[ally.Count() - 1]].Cells[allx[allx.Count() - 1]].Style.BackColor = Color.Green;
+				ally.Insert(ally.Count, snky[m] - 2);
+				Map.Rows[ally[ally.Count() - 1]].Cells[allx[allx.Count() - 1]].Style.BackColor = Color.Green;
+				ally.Insert(ally.Count, snky[m] - 3);
+				Map.Rows[ally[ally.Count() - 1]].Cells[allx[allx.Count() - 1]].Style.BackColor = Color.Green;
+
+				m = m + 1;
+			}
+
+		} catch (Exception ex) {
+			goto Exitry2; // TODO: might not be correct. Was : Exit Try
+		}
+
+        Exitry2:
+
+		this.Map.CurrentCell = this.Map[i1y, i1x];
+	}
+
+    public void layer3()
+    {
+        foreach (DataGridViewRow row in Map.Rows)
+        {
+            foreach(DataGridViewCell cell in row.Cells) //runs really slow, mabye fix?
+                //IT worked fine, mabye a3.1 just sucks?
+            {
+                if (cell.Style.BackColor == Color.Green)
+                {
+                    cell.Value = Properties.Resources.Green;
+                }
+                else if (cell.Style.BackColor == Color.Blue)
+                {
+                    cell.Value = Properties.Resources.Blue;
+                }
+                else if (cell.Style.BackColor == Color.Peru)
+                {
+                    cell.Value = Properties.Resources.Village_Unclaimed1;
+                }
+                else if (cell.Style.BackColor == Color.SaddleBrown)
+                {
+                    cell.Value = Properties.Resources.Horse_Unclaimed;
+                }
+                else if (cell.Style.BackColor == Color.Gray)
+                {
+                    cell.Value = Properties.Resources.Mountain;
+                }
+                else if (cell.Style.BackColor == Color.Purple)
+                {
+                    cell.Value = Properties.Resources.MythHorse_Unclaimed;
+                }
+                else if (cell.Style.BackColor == Color.IndianRed)
+                {
+                    cell.Value = Properties.Resources.Dragonstone_Unclaimed;
+                }
+                else if (cell.Style.BackColor == Color.Gold)
+                {
+                    cell.Value = Properties.Resources.Gold_Unclaimed;
+                }
+            }
+        }
+    }
+    public void layer2()
+    {
+        foreach(DataGridViewRow row in Map.Rows)
+        {
+           foreach(DataGridViewCell cell in row.Cells)
+           {
+               if (cell.Style.BackColor == Color.Green)
+               {
+                   landtiles += 1;
+               }
+               else
+               {
+                   oceeantiles += 1;
+               }
+           }
+        }
+
+
+        if (landtiles > (mapsizehor * mapsizever) * 0.4)
+        {
+            // small islands
+
+            int rando1 = randomizar.Next(7, Convert.ToInt16(oceeantiles / 300)); //bug can occur here if oceantiles goes too low
+
+            for (int i = 0; i <= rando1; i++)
+            {
+                Map.Rows[randomizar.Next(1, mapsizever)].Cells[randomizar.Next(1, mapsizehor)].Style.BackColor = Color.Green;
+            }
+
+            // generate villages
+            int rando2 = randomizar.Next(70, 90);
+
+            for (int i = 0; i <= rando2; i++)
+            {
+                int rand1;
+                int rand2;
+
+                rand1 = randomizar.Next(1, mapsizehor);
+                rand2 = randomizar.Next(1, mapsizever);
+
+                if (Map.Rows[rand2].Cells[rand1].Style.BackColor.Equals(Color.Green))
+                {
+                    Map.Rows[rand2].Cells[rand1].Style.BackColor = Color.Peru;
+                }
+                else
+                {
+                    i = i - 1;
+                }
+            }
+
+            //generate horses
+            int rando3 = randomizar.Next(10, 30);
+
+            for (int i = 0; i <= rando3; i++)
+            {
+                int rand1;
+                int rand2;
+
+                rand1 = randomizar.Next(1, mapsizehor);
+                rand2 = randomizar.Next(1, mapsizever);
+
+                if (Map.Rows[rand2].Cells[rand1].Style.BackColor.Equals(Color.Green))
+                {
+                    Map.Rows[rand2].Cells[rand1].Style.BackColor = Color.SaddleBrown;
+                }
+                else
+                {
+                    i = i - 1;
+                }
+            }
+
+            //generate mountains
+            int rando4 = randomizar.Next(30, 60);
+
+            for (int i = 0; i <= rando4; i++)
+            {
+                int rand1;
+                int rand2;
+
+                rand1 = randomizar.Next(1, mapsizehor);
+                rand2 = randomizar.Next(1, mapsizever);
+
+                if (Map.Rows[rand2].Cells[rand1].Style.BackColor.Equals(Color.Green))
+                {
+                    Map.Rows[rand2].Cells[rand1].Style.BackColor = Color.Gray;
+                }
+                else
+                {
+                    i = i - 1;
+                }
+            }
+            
+            //generate mythicals
+            int rando5 = randomizar.Next(1, 2);
+
+            for (int i = 0; i <= rando5; i++)
+            {
+                int rand1;
+                int rand2;
+
+                rand1 = randomizar.Next(1, mapsizehor);
+                rand2 = randomizar.Next(1, mapsizever);
+
+                if (Map.Rows[rand2].Cells[rand1].Style.BackColor.Equals(Color.Green))
+                {
+                    Map.Rows[rand2].Cells[rand1].Style.BackColor = Color.Purple;
+                }
+                else
+                {
+                    i = i - 1;
+                }
+            }
+
+            //generate dragonstone
+
+            for (int i = 0; i <= 0; i++)
+            {
+                int rand1;
+                int rand2;
+
+                rand1 = randomizar.Next(1, mapsizehor);
+                rand2 = randomizar.Next(1, mapsizever);
+
+                if (Map.Rows[rand2].Cells[rand1].Style.BackColor.Equals(Color.Green))
+                {
+                    Map.Rows[rand2].Cells[rand1].Style.BackColor = Color.IndianRed;
+                }
+                else
+                {
+                    i = i - 1;
+                }
+            }
+
+            //generate native gold
+            for (int i = 0; i <= 1; i++)
+            {
+                int rand1;
+                int rand2;
+
+                rand1 = randomizar.Next(1, mapsizehor);
+                rand2 = randomizar.Next(1, mapsizever);
+
+                if (Map.Rows[rand2].Cells[rand1].Style.BackColor.Equals(Color.Green))
+                {
+                    Map.Rows[rand2].Cells[rand1].Style.BackColor = Color.Gold;
+                }
+                else
+                {
+                    i = i - 1;
+                }
+            }
+
+        }
+        else
+        {
+            Map.Rows.Clear();
+            Map.Columns.Clear();
+            landtiles = 0;
+            oceeantiles = 0;
+            Form1_Load("MAP", new System.EventArgs()); //this reloads form1 so it can be regenerated
+        }
+    }
+
+	private void Map_SelectionChanged(object sender, EventArgs e)
+	{
+		Map.ClearSelection();
+    }
+
+
+    private void MinMap_SelectionChanged(object sender, EventArgs e)
+    {
+        MinMap.ClearSelection();
+    }
+
+    private void button1_Click(object sender, EventArgs e)
+    {
+        generatemap(Convert.ToInt32(textBox1.Text));
+        minimap();
+    }
+
+    //public Form1()
+    //{
+    //    Load += Form1_Load;
+    //}
+}
+
+
+    }
+
